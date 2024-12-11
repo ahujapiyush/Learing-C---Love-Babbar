2262f57271a610c1361b773224623d2c3b409f11
Piyush Ahuja <PIYUSHAHUJA1997@GMAIL.COM>
2024-12-11 11:50:11 +0530

    udated

---
+++ P64.cpp
@@ -0,0 +1,124 @@
+// Understanding Singly Linked list Again
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+    }
+};
+
+void insertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void InsertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void InsertAtPosition(Node *&tail, Node *&head, int d, int position)
+{
+
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    if (temp->next == NULL)
+    {
+        InsertAtTail(tail, d);
+    }
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void deleteNode(Node *&head, int position)
+{
+    // deleting first or start node
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+void print(Node *&head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *node1 = new Node(4);
+    Node *head = node1;
+    Node *tail = node1;
+    cout << node1->data << endl;
+    cout << node1->next << endl;
+
+    insertAtHead(head, 10);
+    print(head);
+
+    insertAtHead(head, 12);
+    print(head);
+
+    InsertAtPosition(tail, head, 22, 1);
+    print(head);
+
+    deleteNode(head, 1);
+    print(head);
+    return 0;
+}
